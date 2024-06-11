05b38cf5a2348f4eb8aa81d27fb12785f8814ddc
ejprietocal <prietoeider1@gmail.com>
2024-06-11 13:47:18 -0500

    fixing anecdotes

--- parte1/anecdotes/src/App.jsx
+++ parte1/anecdotes/src/App.jsx
@@ -1,95 +1,60 @@
 import { useState } from 'react'
 
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const Button = props =>(<button onClick={props.handleClick}>{props.text}</button>)
 
-const Display = props => (
-  <h2>{props.value}</h2>
-)
-
-const StatisticLine = props => {
-  return(
-    <>
-      <tr>
-          <td>{props.text}</td>
-          <td>{props.value}</td>
-      </tr>
-    </>
-  )
-}
-
-const Statics = props =>{
-
-  const total = props.good  + props.neutral + props.bad;
-  const positivos = props.good / total;
-
-  const average =  (props.good  - props.bad) / total;
-
-
-  if(total === 0){
-    return (
-      <>
-        <p>No feedback given</p>
-      </>
-    )
-  }
-  else{
-    return (
-      <>
-        <h2>{props.value}</h2>
-        <table>
-          <StatisticLine text='good' value={props.good} />
-          <StatisticLine text='neutral' value={props.neutral} />
-          <StatisticLine text='bad' value={props.bad} />
-        <tr>
-          <td>All</td>
-          <td>{props.good + props.neutral + props.bad}</td>
-        </tr>
-        <tr>
-          <td>Average</td>
-          <td>{average.toFixed(6)}</td>
-        </tr>
-        <tr>
-          <td>Positive</td>
-          <td>{positivos.toFixed(6)} %</td>
-        </tr>
-        </table>
-      </>
-    ) 
-
-  }
-}
 
 const App = () => {
-  const [good, setValue] = useState(0)
-  const [neutral, setValueNeutral] = useState(0)
-  const [bad, setValueBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
+  const [selected, setSelected] = useState(0)
+  const initialVote = Array.from({length: anecdotes.length},() => 0);
 
-  const setToValue = (props) => {
-    if(props === 'good'){
-      setValue(good + 1)
-    }
-    else if(props === 'neutral'){
-      setValueNeutral(neutral + 1)
-    }
-    else setValueBad(bad + 1)
+  const [vote, setVote] = useState(initialVote);
+
+  const getRandonInt= max =>{
+    return Math.floor(Math.random() * max);
   }
 
+  const setTovalue = () =>{
+       setSelected(getRandonInt(anecdotes.length - 1)); 
+  }
+  const handleVote = (index) =>{
+    setVote(prevVote =>{
+        const newVote =[...prevVote];
+        newVote[index] ++;
+        return newVote;
+    })
+  }
+
+  const max = array => {
+     const maxValue = Math.max(...array);
+     const maxIndex = array.indexOf(maxValue);
+
+     console.log(maxIndex)
+     return maxIndex;   
+  }
   return (
     <div>
-      <Display value={'Give a feedback'} />
-      <Button handleClick={() => setToValue('good')} text="good" />
-      <Button handleClick={() => setToValue('neutral')} text="neutral" />
-      <Button handleClick={() => setToValue('bad')} text="bad" />
-
-      <Statics value={'Statistics'} good={good}  neutral={neutral} bad={bad} />
-
+      <h2>Anecdote of the day</h2>  
+      <p>{anecdotes[selected]}</p>
+      <p>Has {vote[selected]} Votes</p>
+      <Button handleClick={() =>handleVote(selected)} text='vote' />  
+      <Button handleClick={() =>setTovalue('next')} text='next anecdote' />  
+      <h2>Anecdote with most Votes</h2>  
+      <p>{anecdotes[max(vote)]}</p>
+      <p>has {vote[max(vote)]} votes</p>
     </div>
   )
 }
+
 export default App
