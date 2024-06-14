8a5006a4f2b519b630817b127e4a52230da1815b
ejprietocal <prietoeider1@gmail.com>
2024-06-13 21:12:23 -0500

    part2.2

--- parte1/anecdotes/src/main.jsx
+++ parte1/anecdotes/src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-// import App from './App'
-import App from './Anecdotes.1.12'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
